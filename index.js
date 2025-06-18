cc4c29366003b7210fb9bc8ff1cf1bf4d26540f7
Abzal <33022987+abzalramazanov@users.noreply.github.com>
2025-06-18 12:03:08 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ app.use(express.json());
 const TELEGRAM_TOKEN = "7321576020:AAEt-579ibyc5X1BOEQOymyLQ4Sil4pR1tU";
 const TELEGRAM_API = `https://api.telegram.org/bot${TELEGRAM_TOKEN}`;
 const CHAT_ID = -1001517811601; // DevTeam (форум)
-const THREAD_ID = 8282; // Support topic внутри DevTeam
 const USEDESK_TOKEN = "12ff4f2af60aee0fe6869cec6e2c8401df7980b7";
 
 // Для сопоставления message_id и ticket_id
@@ -25,7 +24,6 @@ app.get("/send", async (req, res) => {
   try {
 const tgRes = await axios.post(`${TELEGRAM_API}/sendMessage`, {
   chat_id: CHAT_ID,
-  message_thread_id: THREAD_ID,
   text,
   link_preview_options: { is_disabled: true }
 });
